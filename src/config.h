b26d25db7c52546670a3f5143702066dba8b1f76
Marco Costalba <mcostalba@gmail.com>
2007-09-30 08:26:06 +0200

    qgit-2.0
    Signed-off-by: Marco Costalba <mcostalba@gmail.com>

--- src/config.h
+++ src/config.h
@@ -17,7 +17,7 @@
 #define PACKAGE_TARNAME "qgit"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.0rc2"
+#define PACKAGE_VERSION "2.0"
 
 /* Version number of package */
-#define VERSION "2.0rc2"
+#define VERSION "2.0"
