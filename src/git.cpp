tibirna/qgit

commit d719956cea12349480f29909127f44aef5962eaa

Fetch file history from all branches
Not only current one. This is a regression introduced
during the conversion from git-rev-list to git-log in
Switch to 'git log' instead of 'git rev-list'.

This fixes annotation of merge-recursive.c in git tree
when called from revision 07b45f8c1754

Signed-off-by: Marco Costalba <mcostalba@gmail.com>

src/git.cpp
@@ -1269,6 +1269,8 @@ bool Git::startFileHistory(SCRef sha, SCRef startingFileName, FileHistory* fh) {
 
 	QString newestFileName = getNewestFileName(args, startingFileName);
 	fh->resetFileNames(newestFileName);
+
+	args.clear(); // branches will be added in startRevList()
 	args << "--" << newestFileName;
 	return startRevList(args, fh);
 }

src/git_startup.cpp
@@ -482,8 +482,11 @@ bool Git::startRevList(SCList args, FileHistory* fh) {
 
 	const QString baseCmd("git log --no-color --log-size --parents --boundary --pretty=raw -z");
 	QStringList initCmd(baseCmd.split(' '));
-	if (!isMainHistory(fh))
+	if (!isMainHistory(fh)) {
 	/*
+	   fetch history from all branches so any revision in
+	   main view that changes the file is always found
+
 	   NOTE: we don't use '--remove-empty' option because
 	   in case a file is deleted and then a new file with
 	   the same name is created again in the same directory
@@ -491,7 +494,8 @@ bool Git::startRevList(SCList args, FileHistory* fh) {
 	   the file deletion revision.
 	*/
 		initCmd << QString("-r -m -p --full-index").split(' ');
-	else
+		initCmd << getAllRefSha(BRANCH | RMT_BRANCH);
+	} else
 		initCmd << "--topo-order";
 
 	return startParseProc(initCmd + args, fh, QString());

commit d757f4b3cd86c4604dc81a1f43f0c98a6b8559b4

Fix an issue introduced with history from all branches
Recent patch d719956cea1 introduced an issue in case
of file renames. In that case we don't have to follow
the new file name on all the branches, but only starting
from the revision that renamed the file.

This fix annotation of git file diff-lib.c

Signed-off-by: Marco Costalba <mcostalba@gmail.com>

src/git.cpp
@@ -1270,7 +1270,9 @@ bool Git::startFileHistory(SCRef sha, SCRef startingFileName, FileHistory* fh) {
 	QString newestFileName = getNewestFileName(args, startingFileName);
 	fh->resetFileNames(newestFileName);
 
-	args.clear(); // branches will be added in startRevList()
+	args.clear(); // load history from all the branches
+	args << getAllRefSha(BRANCH | RMT_BRANCH);
+
 	args << "--" << newestFileName;
 	return startRevList(args, fh);
 }

src/git_startup.cpp
@@ -485,9 +485,6 @@ bool Git::startRevList(SCList args, FileHistory* fh) {
 	QStringList initCmd(baseCmd.split(' '));
 	if (!isMainHistory(fh)) {
 	/*
-	   fetch history from all branches so any revision in
-	   main view that changes the file is always found
-
 	   NOTE: we don't use '--remove-empty' option because
 	   in case a file is deleted and then a new file with
 	   the same name is created again in the same directory
@@ -495,7 +492,6 @@ bool Git::startRevList(SCList args, FileHistory* fh) {
 	   the file deletion revision.
 	*/
 		initCmd << QString("-r -m -p --full-index").split(' ');
-		initCmd << getAllRefSha(BRANCH | RMT_BRANCH);
 	}
 	return startParseProc(initCmd + args, fh, QString());
 }