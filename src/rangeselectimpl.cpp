tibirna/qgit

commit b672826c12bde2a2b5f897c38076725092959252

Restore default select of first tag in range dialog
In range dialog select the first tag that is not also
the current HEAD.

After the last patches the default become the first branch,
but this is 99% of cases unuseful because it is the
same of HEAD.

So user ends up almost always tweaking the range dialog
defaults before opening the repository.

With this patch default values are restored to more
sensible ones.

Signed-off-by: Marco Costalba <mcostalba@gmail.com>

src/rangeselectimpl.cpp
@@ -32,16 +32,18 @@ RangeSelectImpl::RangeSelectImpl(QWidget* par, QString* r, bool repoChanged, Git
 	if (!tmp.isEmpty())
 		orl << tmp;
 
+	// as default select first tag that is not also the current HEAD
+	int defIdx = orl.count() - tmp.count();
+	if (!tmp.empty()) {
+		SCRef tagSha(git->getRefSha(tmp.first(), Git::TAG, false));
+		if (!tagSha.isEmpty() && git->checkRef(tagSha, Git::CUR_BRANCH))
+			// in this case set as default tag the next one if any
+			defIdx += (tmp.count() > 1 ? 1 : 0);
+	}
+
 	if (!orl.isEmpty() && orl.last().isEmpty())
 		orl.pop_back();
 
-// 	// check if top tag is current HEAD
-// 	if (!orl.empty()) {
-// 		SCRef tagSha(git->getRefSha(orl.first(), Git::ANY_REF, false));
-// 		if (!tagSha.isEmpty() && git->checkRef(tagSha, Git::CUR_BRANCH))
-// 			// in this case remove from list to avoid an empty view
-// 			orl.pop_front();
-// 	}
 	QString from, to, options;
 
 	if (!repoChanged) {
@@ -60,7 +62,7 @@ RangeSelectImpl::RangeSelectImpl(QWidget* par, QString* r, bool repoChanged, Git
 		comboBoxTo->setEditText(to);
 
 	comboBoxFrom->insertItems(0, orl);
-	idx = repoChanged ? 0 : comboBoxFrom->findText(from);
+	idx = repoChanged ? defIdx : comboBoxFrom->findText(from);
 	if (idx != -1)
 		comboBoxFrom->setCurrentIndex(idx);
 	else

commit ccaeef602b3b476cf80a98aa8737bbb777887d66

Range select: do not select first tag if it's the HEAD
Bug (introduced by b672826c12bd of 2/1/2008) went unoticed because it
shows only if there is one tag, in case many tag are present
code correctly went to the second one.

This patch fixes this corener case.

Signed-off-by: Marco Costalba <mcostalba@gmail.com>

src/rangeselectimpl.cpp
@@ -38,7 +38,7 @@ RangeSelectImpl::RangeSelectImpl(QWidget* par, QString* r, bool repoChanged, Git
 		SCRef tagSha(git->getRefSha(tmp.first(), Git::TAG, false));
 		if (!tagSha.isEmpty() && git->checkRef(tagSha, Git::CUR_BRANCH))
 			// in this case set as default tag the next one if any
-			defIdx += (tmp.count() > 1 ? 1 : 0);
+			defIdx += (tmp.count() > 1 ? 1 : -1);
 	}
 
 	if (!orl.isEmpty() && orl.last().isEmpty())