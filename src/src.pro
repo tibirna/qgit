tibirna/qgit

commit 50d839bcc6f6ce3646b7df7473256113942484b1

Don't hardcode GIT_EXEC_DIR for Windows so much
Use the environment variable %ProgramFiles% which points to the right
program installation directory, regardless of your system locale.

Also escape the backslash to play it safe.

Signed-off-by: Markus Heidelberg <markus.heidelberg@web.de>
Signed-off-by: Marco Costalba <mcostalba@gmail.com>

src/src.pro
@@ -1,6 +1,6 @@
 # Under Windows launch script start_qgit.bat needs the
 # value GIT_EXEC_DIR to be set to the git bin directory
-GIT_EXEC_DIR = "C:\Program Files\Git\bin"
+GIT_EXEC_DIR = "$(ProgramFiles)\\Git\\bin"
 
 # Under Windows uncomment following line to enable console messages
 #CONFIG += ENABLE_CONSOLE_MSG

commit c64ce18178304b5311da92eef246a1a3cf3b6fd5

Immediately evaluate the ProgramFiles environment variable
Commit 50d839b (Don't hardcode GIT_EXEC_DIR for Windows so much,
2009-04-18) broke 'make install' on Windows. Somehow the current working
directory was prepended to the install path given with target.path, when
$(ProgramFiles) should be evaluated during the call of 'make'.
This seems to be a qmake bug with generating the Makefile.

Signed-off-by: Markus Heidelberg <markus.heidelberg@web.de>
Signed-off-by: Marco Costalba <mcostalba@gmail.com>

src/src.pro
@@ -1,6 +1,6 @@
 # Under Windows launch script start_qgit.bat needs the
 # value GIT_EXEC_DIR to be set to the git bin directory
-GIT_EXEC_DIR = "$(ProgramFiles)\\Git\\bin"
+GIT_EXEC_DIR = "$$(ProgramFiles)\\Git\\bin"
 
 # Under Windows uncomment following line to enable console messages
 #CONFIG += ENABLE_CONSOLE_MSG