e2046fb57b62af45f3c7842d9b42fa7d2f8e2f90
Cristian Tibirna <cristian@tibirna.org>
2012-08-27 16:31:45 -0400

    next version is 2.5

--- src/config.h
+++ src/config.h
@@ -17,7 +17,7 @@
 #define PACKAGE_TARNAME "qgit"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.3"
+#define PACKAGE_VERSION "2.5"
 
 /* Version number of package */
-#define VERSION "2.3"
+#define VERSION "2.5"
