2d0c3b0a9138e8e14d7f2c147fa7267ff5e23282
Cristian Tibirna <cristian@tibirna.org>
2016-03-19 19:17:54 -0400

    Version 2.6

--- src/config.h
+++ src/config.h
@@ -17,7 +17,7 @@
 #define PACKAGE_TARNAME "qgit"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.5"
+#define PACKAGE_VERSION "2.6"
 
 /* Version number of package */
-#define VERSION "2.5"
+#define VERSION "2.6"
