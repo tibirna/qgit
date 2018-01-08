845d41e9e86de9743b066f7896ff61c583b1ba21
Pavel Karelin <hkarel@yandex.ru>
2018-01-08 14:20:44 +0300

    Updated module  break_point.h

--- src/break_point.h
+++ src/break_point.h
@@ -1,8 +1,33 @@
 /* clang-format off */
-/****************************************************************************
-  Author of idea: Fedorin Denis
-  The module implemented the program breakpoint
-****************************************************************************/
+/*****************************************************************************
+  The MIT License
+
+  Copyright © 2013 Pavel Karelin (hkarel), <hkarel@yandex.ru>
+  Author of idea: Denis Fedorin
+
+  Permission is hereby granted, free of charge, to any person obtaining
+  a copy of this software and associated documentation files (the
+  "Software"), to deal in the Software without restriction, including
+  without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so, subject to
+  the following conditions:
+
+  The above copyright notice and this permission notice shall be included
+  in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  ---
+
+  The module implemented the program breakpoint.
+
+*****************************************************************************/
 
 #pragma once
 
@@ -38,6 +63,8 @@ int main ()
   #if defined(_MSC_VER) || defined(__MINGW32__)
     #include <intrin.h>
     #define break_point  __debugbreak();
+  #elif defined(__arm__)
+    #define break_point
   #else
     #include <stdio.h>
     #include <signal.h>
