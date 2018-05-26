e535dfac0add68114cb3d7e07fb47dd2c19200c8
Cristian Tibirna <tibirna@kde.org>
2018-05-26 10:10:14 -0400

    Set version 2.8

--- src/config.h
+++ src/config.h
@@ -17,7 +17,7 @@
 #define PACKAGE_TARNAME "qgit"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.7"
+#define PACKAGE_VERSION "2.8"
 
 /* Version number of package */
-#define VERSION "2.7"
+#define VERSION "2.8"
