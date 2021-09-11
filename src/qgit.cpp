e7ec91fbc00a3b6682ac188fe0fde9bc87da3a01
Sebastian Pipping <sebastian@pipping.org>
2021-09-11 19:33:50 +0200

    qgit.cpp: Support --help, --help-all and --version

--- src/qgit.cpp
+++ src/qgit.cpp
@@ -1,10 +1,13 @@
 /*
 	Author: Marco Costalba (C) 2005-2007
+	Author: Sebastian Pipping (C) 2021
 
 	Copyright: See COPYING file that comes with this distribution
 
 */
+#include <QCommandLineParser>
 #include <QSettings>
+#include "config.h" // defines PACKAGE_VERSION
 #include "common.h"
 #include "mainimpl.h"
 
@@ -21,6 +24,29 @@ int main(int argc, char* argv[]) {
 	app.setAttribute(Qt::AA_UseHighDpiPixmaps, true);
 	QCoreApplication::setOrganizationName(ORG_KEY);
 	QCoreApplication::setApplicationName(APP_KEY);
+
+	QCommandLineParser parser;
+	QCoreApplication::setApplicationVersion(PACKAGE_VERSION);
+	parser.addHelpOption();
+	parser.addVersionOption();
+	parser.setApplicationDescription("QGit, a Git GUI viewer");
+	parser.addPositionalArgument("git-log-args",
+		"Arguments forwarded to \"git log\"; for example:\n"
+		"   qgit --no-merges\n"
+		"   qgit v2.6.18.. include/scsi \\\n"
+		"                  drivers/scsi\n"
+		"   qgit --since=\"2 weeks ago\" -- kernel/\n"
+		"   qgit -r --name-status release..test\n"
+		"See \"man git-log\" for details.",
+		"[git-log-args]");
+
+	parser.parse(app.arguments());
+
+	if (parser.isSet("help")
+			|| parser.isSet("help-all")
+			|| parser.isSet("version")) {
+		parser.process(app.arguments());  // exits the process
+	}
 
 	/* On Windows msysgit exec directory is set up
 	 * during installation so to always find git.exe
