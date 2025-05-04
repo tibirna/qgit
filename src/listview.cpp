tibirna/qgit

commit 6f9477794c86cbc2b9a36cdd0f68cef90725e276

Replace deprecated QFontMetrics::width by QFontMetrics::horizontalAdvance of Qt >=5.11
Docs:
- https://doc.qt.io/archives/qt-4.8/qfontmetrics.html#width-3
- https://doc.qt.io/qt-5/qfontmetrics.html#horizontalAdvance-1

Addresses this compiler warning:
warning: ‘int QFontMetrics::width(const QString&, int) const’ is deprecated: Use QFontMetrics::horizontalAdvance [-Wdeprecated-declarations]

src/listview.cpp
@@ -352,7 +352,7 @@ QPixmap ListView::pixmapFromSelection(const QStringList &revs, const QString &re
 		QStyleOptionViewItem o(opt);
 		QString dummy;
 		getTagMarkParams(dummy, o, refTypeFromName(ref), false);
-		painter.fillRect(0, 0, fm.width(ref)+2*spacing, height, o.palette.window());
+		painter.fillRect(0, 0, fm.horizontalAdvance(ref)+2*spacing, height, o.palette.window());
 		painter.drawText(spacing, fm.ascent()+1, ref);
 		row = 1;
 	}

commit 7e061bedafa8ffb770e5e05c74f274c388199971

listview.cpp: fix QFontMetrics
Fix a breakage introduced by https://github.com/tibirna/qgit/commit/6f9477794c86cbc2b9a36cdd0f68cef90725e276

src/listview.cpp
@@ -354,7 +354,11 @@ QPixmap ListView::pixmapFromSelection(const QStringList &revs, const QString &re
 		QStyleOptionViewItem o(opt);
 		QString dummy;
 		getTagMarkParams(dummy, o, refTypeFromName(ref), false);
+#if QT_VERSION >= QT_VERSION_CHECK(5, 11, 0)
 		painter.fillRect(0, 0, fm.horizontalAdvance(ref)+2*spacing, height, o.palette.window());
+#else
+		painter.fillRect(0, 0, fm.width(ref)+2*spacing, height, o.palette.window());
+#endif
 		painter.drawText(spacing, fm.ascent()+1, ref);
 		row = 1;
 	}