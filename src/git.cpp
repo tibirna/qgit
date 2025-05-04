tibirna/qgit

commit c4c764215952e0a8780fb990ee77868fc38bee56

Replace deprecated QString::sprintf by QString::asprintf of Qt >=5.5
Docs:
- https://doc.qt.io/qt-5/qstring-obsolete.html#sprintf
- https://doc.qt.io/qt-5/qstring.html#asprintf

Addresses this compiler warning:
warning: ‘QString& QString::sprintf(const char*, ...)’ is deprecated: Use asprintf(), arg() or QTextStream instead [-Wdeprecated-declarations]

src/git.cpp
@@ -2419,9 +2419,9 @@ void Git::on_loaded(FileHistory* fh, ulong byteSize, int loadTime,
                         ulong kb = byteSize / 1024;
                         double mbs = (double)byteSize / fh->loadTime / 1000;
                         QString tmp;
-                        tmp.sprintf("Loaded %i revisions  (%li KB),   "
-                                    "time elapsed: %i ms  (%.2f MB/s)",
-                                    fh->revs.count(), kb, fh->loadTime, mbs);
+                        tmp.asprintf("Loaded %i revisions  (%li KB),   "
+                                     "time elapsed: %i ms  (%.2f MB/s)",
+                                     fh->revs.count(), kb, fh->loadTime, mbs);
 
                         if (!tryFollowRenames(fh))
                                 emit loadCompleted(fh, tmp);

commit 02565959440ff0ed50b2f5a71fb0a305c383deb7

git.cpp: fix asprintf
Fixes a breakaga introduced by https://github.com/tibirna/qgit/commit/c4c764215952e0a8780fb990ee77868fc38bee56

src/git.cpp
@@ -31,6 +31,12 @@
 
 #define GIT_LOG_FORMAT "%m%HX%PX%n%cn<%ce>%n%an<%ae>%n%at%n%s%n"
 
+#if QT_VERSION >= QT_VERSION_CHECK(5, 5, 0)
+    #define QT_ASPRINTF asprintf
+#else
+    #define QT_ASPRINTF sprintf
+#endif
+
 // Used on init() for reading parameters once;
 // It's OK to be unique among qgit windows.
 static bool startup = true;
@@ -2436,7 +2442,7 @@ void Git::on_loaded(FileHistory* fh, ulong byteSize, int loadTime,
                         ulong kb = byteSize / 1024;
                         double mbs = (double)byteSize / fh->loadTime / 1000;
                         QString tmp;
-                        tmp.asprintf("Loaded %i revisions  (%li KB),   "
+                        tmp.QT_ASPRINTF("Loaded %i revisions  (%li KB),   "
                                      "time elapsed: %i ms  (%.2f MB/s)",
                                      fh->revs.count(), kb, fh->loadTime, mbs);
 