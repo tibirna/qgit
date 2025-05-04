tibirna/qgit

commit d4b85573702fc19a4b32178ffe58f9f5cc5fd73e

Replace deprecated QWheelEvent::delta() by QWheelEvent::angleDelta().y()
Docs:
- https://doc.qt.io/archives/qt-4.8/qwheelevent.html#delta
- https://doc.qt.io/qt-5/qwheelevent.html#angleDelta

Addresses this compiler warning:
warning: ‘int QWheelEvent::delta() const’ is deprecated: Use angleDelta() [-Wdeprecated-declarations]

src/mainimpl.cpp
@@ -701,7 +701,7 @@ bool MainImpl::eventFilter(QObject* obj, QEvent* ev) {
 		if (e->modifiers() == Qt::AltModifier) {
 
 			int idx = tabWdg->currentIndex();
-			if (e->delta() < 0)
+			if (e->angleDelta().y() < 0)
 				idx = (++idx == tabWdg->count() ? 0 : idx);
 			else
 				idx = (--idx < 0 ? tabWdg->count() - 1 : idx);

src/smartbrowse.cpp
@@ -224,7 +224,7 @@ bool SmartBrowse::eventFilter(QObject *obj, QEvent *event) {
 	if (sb && t == QEvent::Wheel && sb->orientation() == Qt::Vertical) {
 
 		QWheelEvent* we = static_cast<QWheelEvent*>(event);
-		if (wheelRolled(we->delta(), visibilityFlags()))
+		if (wheelRolled(we->angleDelta().y(), visibilityFlags()))
 			return true; // filter event out
 	}
 	return QObject::eventFilter(obj, event);

commit 0734156792766f81d6f9ecc85e3d1ecadd2e41ca

Fix for QWheelEvent::delta()
Fix a breakage introduced by https://github.com/tibirna/qgit/commit/d4b85573702fc19a4b32178ffe58f9f5cc5fd73e

src/mainimpl.cpp
@@ -723,7 +723,11 @@ bool MainImpl::eventFilter(QObject* obj, QEvent* ev) {
 		if (e->modifiers() == Qt::AltModifier) {
 
 			int idx = tabWdg->currentIndex();
+#if QT_VERSION < QT_VERSION_CHECK(5, 0, 0)
+			if (e->delta() < 0)
+#else
 			if (e->angleDelta().y() < 0)
+#endif
 				idx = (++idx == tabWdg->count() ? 0 : idx);
 			else
 				idx = (--idx < 0 ? tabWdg->count() - 1 : idx);

src/smartbrowse.cpp
@@ -224,7 +224,11 @@ bool SmartBrowse::eventFilter(QObject *obj, QEvent *event) {
 	if (sb && t == QEvent::Wheel && sb->orientation() == Qt::Vertical) {
 
 		QWheelEvent* we = static_cast<QWheelEvent*>(event);
+#if QT_VERSION < QT_VERSION_CHECK(5, 0, 0)
+		if (wheelRolled(we->delta(), visibilityFlags()))
+#else
 		if (wheelRolled(we->angleDelta().y(), visibilityFlags()))
+#endif
 			return true; // filter event out
 	}
 	return QObject::eventFilter(obj, event);