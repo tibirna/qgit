tibirna/qgit

commit 1cb9acfa385e81cb9249e444603d7eff5be64c92

port to qt6: QRegExp -> QRegularExpression

src/mainimpl.cpp
@@ -273,10 +273,10 @@ void MainImpl::ActExternalDiff_activated() {
 	}
 }
 
-const QRegExp MainImpl::emptySha("0*");
+const QRegularExpression MainImpl::emptySha("0*");
 
 QString MainImpl::copyFileToDiffIfNeeded(QStringList* filenames, QString sha) {
-	if (emptySha.exactMatch(sha))
+	if (emptySha.match(sha).hasMatch())
 	{
 		return QString(curDir + "/" + rv->st.fileName());
 	}

src/mainimpl.h
@@ -8,7 +8,7 @@
 #define MAINIMPL_H
 
 #include <QProcess>
-#include <QRegExp>
+#include <QRegularExpression>
 #include <QDir>
 #include "exceptionmanager.h"
 #include "common.h"
@@ -185,9 +185,9 @@ private:
 	QString startUpDir;
 	QString startUpFile;
 	QString textToFind;
-	QRegExp shortLogRE;
-	QRegExp longLogRE;
-	static const QRegExp emptySha;
+	QRegularExpression shortLogRE;
+	QRegularExpression longLogRE;
+	static const QRegularExpression emptySha;
 	QMap<QString, QVariant> revision_variables; // variables used in generic input dialogs
 	bool setRepositoryBusy;
 

src/commitimpl.cpp
@@ -8,7 +8,7 @@
 #include <QTextCodec>
 #include <QSettings>
 #include <QMenu>
-#include <QRegExp>
+#include <QRegularExpression>
 #include <QDir>
 #include <QMessageBox>
 #include <QInputDialog>
@@ -191,8 +191,8 @@ bool CommitImpl::checkFiles(SList selFiles) {
 bool CommitImpl::checkMsg(QString& msg) {
 
 	msg = textEditMsg->toPlainText();
-	msg.remove(QRegExp("(^|\\n)\\s*#[^\\n]*")); // strip comments
-	msg.replace(QRegExp("[ \\t\\r\\f\\v]+\\n"), "\n"); // strip line trailing cruft
+	msg.remove(QRegularExpression("(^|\\n)\\s*#[^\\n]*")); // strip comments
+	msg.replace(QRegularExpression("[ \\t\\r\\f\\v]+\\n"), "\n"); // strip line trailing cruft
 	msg = msg.trimmed();
 	if (msg.isEmpty()) {
 		QMessageBox::warning(this, "Commit changes - QGit",

src/fileview.cpp
@@ -98,7 +98,7 @@ bool FileView::eventFilter(QObject* obj, QEvent* e) {
 	if (e->type() == QEvent::ToolTip && obj == lw) {
 		QHelpEvent* h = static_cast<QHelpEvent*>(e);
 		int id = fileTab->textEditFile->itemAnnId(lw->itemAt(h->pos()));
-		QRegExp re;
+		QRegularExpression re;
 		SCRef sha(fileTab->histListView->shaFromAnnId(id));
 		SCRef d(git->getDesc(sha, re, re, false, model()));
 		lw->setToolTip(d);

src/git.cpp
@@ -12,7 +12,7 @@
 #include <QFile>
 #include <QImageReader>
 #include <QPalette>
-#include <QRegExp>
+#include <QRegularExpression>
 //#include <QSet> //CT TODO remove
 #include <QSettings>
 #include <QTextCodec>
@@ -420,8 +420,8 @@ const QString Git::getTagMsg(SCRef sha) {
 	if (!rf.tagMsg.isEmpty())
 		return rf.tagMsg;
 
-	QRegExp pgp("-----BEGIN PGP SIGNATURE*END PGP SIGNATURE-----",
-	            Qt::CaseSensitive, QRegExp::Wildcard);
+	QRegularExpression pgp("-----BEGIN PGP SIGNATURE*END PGP SIGNATURE-----",
+	            Qt::CaseSensitive, QRegularExpression::Wildcard);
 
 	if (!rf.tagObj.isEmpty()) {
 		QString ro;
@@ -1001,7 +1001,7 @@ const QString Git::getNewCommitMsg() {
 		return "";
 	}
 	QString status = c->longLog();
-	status.prepend('\n').replace(QRegExp("\\n([^#\\n]?)"), "\n#\\1"); // comment all the lines
+	status.prepend('\n').replace(QRegularExpression("\\n([^#\\n]?)"), "\n#\\1"); // comment all the lines
 
 	if (isMergeHead) {
 		QFile file(QDir(gitDir).absoluteFilePath("MERGE_MSG"));
@@ -1018,7 +1018,7 @@ const QString Git::getNewCommitMsg() {
 }
 
 //CT TODO utility function; can go elsewhere
-const QString Git::colorMatch(SCRef txt, QRegExp& regExp) {
+const QString Git::colorMatch(SCRef txt, QRegularExpression& regExp) {
 
 	QString text = qt4and5escaping(txt);
 
@@ -1051,7 +1051,7 @@ const QString Git::formatList(SCList sl, SCRef name, bool inOneLine) {
 	return ls;
 }
 
-const QString Git::getDesc(SCRef sha, QRegExp& shortLogRE, QRegExp& longLogRE,
+const QString Git::getDesc(SCRef sha, QRegularExpression& shortLogRE, QRegularExpression& longLogRE,
                            bool showHeader, FileHistory* fh) {
 
 	if (sha.isEmpty())
@@ -1116,7 +1116,7 @@ const QString Git::getDesc(SCRef sha, QRegExp& shortLogRE, QRegExp& longLogRE,
 	// sha if there isn't a leading trailing space or an open parenthesis and,
 	// in that case, before the space must not be a ':' character.
 	// It's an ugly heuristic, but seems to work in most cases.
-	QRegExp reSHA("..[0-9a-f]{21,40}|[^:][\\s(][0-9a-f]{6,20}", Qt::CaseInsensitive);
+	QRegularExpression reSHA("..[0-9a-f]{21,40}|[^:][\\s(][0-9a-f]{6,20}", Qt::CaseInsensitive);
 	reSHA.setMinimal(false);
 	int pos = 0;
 	while ((pos = text.indexOf(reSHA, pos)) != -1) {
@@ -1293,7 +1293,7 @@ const QString Git::getNewestFileName(SCList branches, SCRef fileName) {
 void Git::getFileFilter(SCRef path, ShaSet& shaSet) const {
 
 	shaSet.clear();
-	QRegExp rx(path, Qt::CaseInsensitive, QRegExp::Wildcard);
+	QRegularExpression rx(path, Qt::CaseInsensitive, QRegularExpression::Wildcard);
 	FOREACH (ShaVect, it, revData->revOrder) {
 
 		if (!revsFiles.contains(*it))

src/git.h
@@ -11,7 +11,7 @@
 #include "common.h"
 
 template <class, class> struct QPair;
-class QRegExp;
+class QRegularExpression;
 class QTextCodec;
 class Annotate;
 //class DataLoader;
@@ -98,7 +98,7 @@ public:
 	bool getTree(SCRef ts, TreeInfo& ti, bool wd, SCRef treePath);
 	static const QString getLocalDate(SCRef gitDate);
 	const QString getCurrentBranchName() const {return curBranchName;}
-	const QString getDesc(SCRef sha, QRegExp& slogRE, QRegExp& lLogRE, bool showH, FileHistory* fh);
+	const QString getDesc(SCRef sha, QRegularExpression& slogRE, QRegularExpression& lLogRE, bool showH, FileHistory* fh);
 	const QString getLastCommitMsg();
 	const QString getNewCommitMsg();
 	const QString getLaneParent(SCRef fromSHA, int laneNum);
@@ -247,7 +247,7 @@ private:
 	const QStringList getOthersFiles();
 	const QStringList getOtherFiles(SCList selFiles, bool onlyInIndex);
 	const QString getNewestFileName(SCList args, SCRef fileName);
-	static const QString colorMatch(SCRef txt, QRegExp& regExp);
+	static const QString colorMatch(SCRef txt, QRegularExpression& regExp);
 	void appendFileName(RevFile& rf, SCRef name, FileNamesLoader& fl);
 	void flushFileNames(FileNamesLoader& fl);
 	void populateFileNamesMap();

src/inputdialog.cpp
@@ -47,7 +47,7 @@ public:
 	void fixup(QString& input) const;
 	State validate(QString & input, int & pos) const;
 private:
-	const QRegExp invalid;
+	const QRegularExpression invalid;
 	bool allowEmpty;
 };
 
@@ -89,7 +89,7 @@ InputDialog::InputDialog(const QString &cmd, const VariableMap &variables,
 	this->setWindowTitle(title);
 	QGridLayout *layout = new QGridLayout(this);
 
-	QRegExp re("%(([a-z_]+)([[]([a-z ,]+)[]])?:)?([^%=]+)(=[^%]+)?%");
+	QRegularExpression re("%(([a-z_]+)([[]([a-z ,]+)[]])?:)?([^%=]+)(=[^%]+)?%");
 	int start = 0;
 	int row = 0;
 	while ((start = re.indexIn(cmd, start)) != -1) {

src/listview.cpp
@@ -1272,7 +1272,7 @@ bool ListViewProxy::filterAcceptsRow(int source_row, const QModelIndex&) const {
 
 int ListViewProxy::setFilter(bool isOn, bool h, SCRef fl, int cn, ShaSet* s) {
 
-	filter = QRegExp(fl, Qt::CaseInsensitive, QRegExp::Wildcard);
+	filter = QRegularExpression(fl, Qt::CaseInsensitive, QRegularExpression::Wildcard);
 	colNum = cn;
 	if (s)
 		shaSet = *s;

commit d4b532ed5bfd15888c82f4c0a0d63383276bc094

Make external diff command work again
.. by fixing SHA1 inspection in method copyFileToDiffIfNeeded
where any SHA1 matched regex pattern "0*".

That led the external diff feature to always compare the working
directory state of a file against itself rather than comparing
the version from a given commit against the version from the commit'
parent.

Regression from commit 1cb9acfa385e81cb9249e444603d7eff5be64c92

src/mainimpl.cpp
@@ -291,10 +291,8 @@ void MainImpl::ActExternalDiff_activated() {
 	}
 }
 
-const QRegularExpression MainImpl::emptySha("0*");
-
 QString MainImpl::copyFileToDiffIfNeeded(QStringList* filenames, QString sha) {
-	if (emptySha.match(sha).hasMatch())
+	if (sha == QGit::ZERO_SHA)
 	{
 		return QString(curDir + "/" + rv->st.fileName());
 	}

src/mainimpl.h
@@ -187,7 +187,6 @@ private:
 	QString textToFind;
 	QRegularExpression shortLogRE;
 	QRegularExpression longLogRE;
-	static const QRegularExpression emptySha;
 	QMap<QString, QVariant> revision_variables; // variables used in generic input dialogs
 	bool setRepositoryBusy;
 